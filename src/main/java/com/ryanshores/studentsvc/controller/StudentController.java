ff32de5ec43ab334493de0fe2a914ac802657290
ryanshores <ryan.shores@me.com>
2024-02-21 14:00:08 -0600

    adds controller with exception handling

---
+++ src/main/java/com/ryanshores/studentsvc/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.ryanshores.studentsvc.controller;
+
+import com.ryanshores.studentsvc.model.Student;
+import com.ryanshores.studentsvc.model.exception.StudentNotFoundException;
+import com.ryanshores.studentsvc.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService service;
+
+    private final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+    @GetMapping("/students/{id}")
+    Student getStudent(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PostMapping("/students/{id}")
+    Student updateStudent(@PathVariable Long id,
+                          @RequestBody Student student) {
+        // service.updateStudent(student);
+        return service.getById(id);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    void notFound(StudentNotFoundException exception) {
+        logger.error("Not Found", exception);
+    }
+}
